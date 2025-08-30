4b06461185890190101b40e90c68f91a77a35fcf
Rahul Patil <patilr2160@gmail.com>
2025-08-30 15:32:37 +0530

    String a is added

--- FirstJavaProject/src/com/velocity/git/practice/Class2.java
+++ FirstJavaProject/src/com/velocity/git/practice/Class2.java
@@ -5,8 +5,8 @@ public class Class2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		
-		System.out.println("Second Class");
+		String a = "Second Class";
+		System.out.println(a);
 
 	}
 
