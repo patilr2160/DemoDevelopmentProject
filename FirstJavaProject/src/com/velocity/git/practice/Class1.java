beb0d3b2bfde69e1b426b3d23758ea70cb99f46e
Rahul Patil <patilr2160@gmail.com>
2025-08-30 15:23:22 +0530

    First Project. Two practice classes

---
+++ FirstJavaProject/src/com/velocity/git/practice/Class1.java
@@ -0,0 +1,13 @@
+package com.velocity.git.practice;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+		
+		String a = "Hello World";
+		
+		System.out.println(a);
+
+	}
+
+}
---
+++ FirstJavaProject/src/com/velocity/git/practice/Class2.java
@@ -0,0 +1,13 @@
+package com.velocity.git.practice;
+
+public class Class2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("Second Class");
+
+	}
+
+}
